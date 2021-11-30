3b27d57af3270933b779428f3ddee02292c5dcd7
marceloicampos <marcelo.ildefonso@hotmail.com>
2021-11-30 11:40:35 -0300

    fixing logo on build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <img src="https://raw.githubusercontent.com/marceloicampos/ignite-desafio01-trilha-reactjs/9c26167e9ed4dbe123eb309153d3a83d169144ef/public/logo.svg" alt="to.do"/>
       </div>
     </header>
   )
